1ee4e8ea8b8b27ce857bfcd749fcf268a4c8123a
michiOdk <Michi.odk@eduge.ch>
2021-02-05 20:27:18 +0100

    Chapter 1 Rendu
    Oubli de commit le 01.02.2021 alors reprise de la précédente version pour faire la responsivité du site

--- index.php
+++ index.php
@@ -10,31 +10,60 @@ Version: 1.0
 <!DOCTYPE html>
 
 <html lang="fr">
-    <head>
-        <title>Home</title>
-        <meta charset="UTF-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Bootstrap CSS  -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-        <!-- My CSS  -->
-        <link rel="stylesheet" href="./css/mainCss.css">
-    </head>
-    <body>
-        <!-- header -->
-       <nav class="navbar-nav bg-gradient-primary accordion">
-        <ul>
-            <li class="nav-item"><i class="fas fa-home"></i><a class="collapse-item" href="index.php">Home</a>
-            <i class="fas fa-clone"></i><a class="collapse-item" href="./php/post.php">Post</a></li>
-        </ul>
-       </nav>
-       <section></section>
-       <article></article>
-        <!-- form action= lien method = post/get-->
-    </body>
-        <!-- FontAwesome kitCode  -->
-         <script src="https://kit.fontawesome.com/b49b3eeefb.js" crossorigin="anonymous"></script>
-        <!-- Bootstrap JS  -->
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+<head>
+    <title>Home</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS  -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <!-- My CSS  -->
+    <link rel="stylesheet" href="./css/mainCss.css">
+</head>
+
+<body>
+    <!-- header -->
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+
+                        <a class="collapse-item nav-link active" href="../index.php"><i class="fas fa-home"></i> Home</a>
+                    </li>
+                    <li class="nav-item">
+
+                        <a class="collapse-item nav-link " href="./php/post.php"><i class="fas fa-clone"></i> Post</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+    <h1 style="text-align: center;">Bienvenu</h1>
+    <div class="container mt-5 d-flex justify-content-center">
+        <div class="card p-3">
+            <div class="d-flex align-items-center">
+                <div class="image"> <img src="img/chitanda.jpg" class="rounded" width="155"> </div>
+                <div class="ml-3 w-100">
+                    <h4 class="mb-0 mt-0">User</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- form action= lien method = post/get-->
+</body>
+<!-- FontAwesome kitCode  -->
+<script src="https://kit.fontawesome.com/b49b3eeefb.js" crossorigin="anonymous"></script>
+<!-- Bootstrap JS  -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
 </html>
